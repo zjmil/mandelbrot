d1bc6708505d3495d86c1c6f3f5ce661ecdc79ff
Zach Miller <zach@zjmil.org>
2021-05-22 11:27:40 -0400

    style

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,9 +6,10 @@
 
 #include <SDL.h>
 
-#define STATIC_ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-void fatal(const char *fmt, ...) {
+void fatal(const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
 
@@ -26,13 +27,11 @@ bool approx(float a, float b)
 
 void calc_iterations(SDL_Color *pixels, int width, int height, SDL_Color *colors, int ncolors)
 {
-    int max_iterations = 1000;
-    int max_periods = 20;
+    int max_iterations = 1000, max_periods = 20;
     float xmin = -2.5, xmax = 1.0, ymin = -1.0, ymax = 1.0;
 
     float xrange = xmax - xmin, yrange = ymax - ymin;
     float xdelta = xrange / width, ydelta = yrange / height;
-
 
     for (int py = 0; py < height; py++) {
         float y0 = ymin + py * ydelta;
@@ -76,16 +75,15 @@ void calc_iterations(SDL_Color *pixels, int width, int height, SDL_Color *colors
 
 int main(int argc, char *argv[])
 {
-    int width = 700;
-    int height = 400;
+    int width = 700, height = 400;
 
-    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
         fatal("Failed initializing SDL: %s\n", SDL_GetError());
     }
 
     SDL_Window *window = SDL_CreateWindow(
             "Mandelbrot",
-            SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
             width, height,
             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
@@ -108,7 +106,6 @@ int main(int argc, char *argv[])
         fatal("Failed to create texture: %s", SDL_GetError());
     }
 
-
     SDL_Color colors[] = {{66, 30, 15, 255},
                           {25, 7, 26, 255},
                           {9, 1, 47, 255},
@@ -125,10 +122,9 @@ int main(int argc, char *argv[])
                           {204, 128, 0, 255},
                           {153, 87, 0, 255},
                           {106, 52, 3, 255}};
-    size_t ncolors = STATIC_ARRAY_SIZE(colors);
+    size_t ncolors = ARRAY_SIZE(colors);
 
-    SDL_Color *pixels = calloc(sizeof(SDL_Color), width * height);
-
+    SDL_Color *pixels = calloc(sizeof(*pixels), width * height);
 
     // clear the screen
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
